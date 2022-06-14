87ebd508e9852d4a1766a3f56a7a8a926da88d40
Ryan Cabral <cabral.reis23@gmail.com>
2022-06-14 09:41:39 -0300

    initial commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe2-_VcifsFTq9kPMv0wSHV0dBeaQvyVjWtg&usqp=CAU'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com',
+      avatarUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe2-_VcifsFTq9kPMv0wSHV0dBeaQvyVjWtg&usqp=CAU'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com',
+      avatarUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe2-_VcifsFTq9kPMv0wSHV0dBeaQvyVjWtg&usqp=CAU')
+};
